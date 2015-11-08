bfac59646cac56e0e970d21cbbe3449fe1984ac5
Leo McArdle <leomcardle@gmail.com>
2015-11-08 16:23:36 +0000

    half-baked frontend stuff

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/thanks/:id', function(req, res, next) {
+  res.render('thanks', { reciever: 'Dino', message: 'Thx', sender: 'Foxy', id: req.params.id })
+})
 
 module.exports = router;
